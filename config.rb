3bef4b491ea3d68bcf3dd283c0f94df0c332ae3f
Wcc723 <agito723@gmail.com>
2014-09-18 10:19:51 +0800

    Casper html5 template

---
+++ config.rb
@@ -0,0 +1,28 @@
+# Addition other sass libary if require:
+# additional_import_paths = ""
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :expanded
+sourcemap = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = false
+sass_options = {:debug_info=>false}
+
+line_comments = false
+
+preferred_syntax = :sass
+fireapp_coffeescripts_dir = "coffeescripts"
+fireapp_coffeescript_options = {:bare=>false}
+fireapp_livescripts_dir = "livescripts"
+fireapp_livescript_options = {:bare=>false}
+fireapp_minifyjs_on_build = false
+fireapp_always_report_on_build = true
+sass_options = {:debug_info=>false}
