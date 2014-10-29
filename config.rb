e950c27cc0b73c6cc06dad775ea307a90bb6b3ef
Wcc723 <agito723@gmail.com>
2014-10-29 14:55:13 +0800

    update html5 template 1. sass, js, images folder move to assets 2. new sturcture for sass

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "assets/stylesheets"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
 
 output_style = :expanded
 sourcemap = false
@@ -19,9 +19,9 @@ sass_options = {:debug_info=>false}
 line_comments = false
 
 preferred_syntax = :sass
-fireapp_coffeescripts_dir = "coffeescripts"
+fireapp_coffeescripts_dir = "assets/coffeescripts"
 fireapp_coffeescript_options = {:bare=>false}
-fireapp_livescripts_dir = "livescripts"
+fireapp_livescripts_dir = "assets/livescripts"
 fireapp_livescript_options = {:bare=>false}
 fireapp_minifyjs_on_build = false
 fireapp_always_report_on_build = true
